edfe796916feb054d20b69eb7e56c983b04aaf00
mathe <matheus.gonzaga.krebs@gmail.com>
2023-09-26 23:10:23 -0300

    pronto

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const Isenha = document.querySelector(".senha");
 const Itel = document.querySelector(".tel");
 
 function cadastrar(){
-    fetch("http://localhost:8080/cadastrar",{
+    fetch("http://localhost:8080/usuarios",{
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
